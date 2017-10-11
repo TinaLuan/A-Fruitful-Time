bead4c0fa76d78430f0e464ee228746dbd04c596
TinaLuan <tinaluan9794@gmail.com>
2017-10-11 18:53:16 +1100

    textures for walls

---
+++ Assets/Scripts/MapScripts/WallScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallScript : MonoBehaviour {
+	
+	public Texture texture;
+
+
+	// Use this for initialization
+	void Start () {
+		
+		MeshRenderer renderer = this.gameObject.GetComponent<MeshRenderer>();
+		renderer.material.shader = Shader.Find("Unlit/WallShader");
+		renderer.material.mainTexture = texture;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
