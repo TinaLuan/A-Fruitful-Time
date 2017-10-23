577b1fc332bbc2299adf4a3d84af1d9c91ad5fc1
TinaLuan <tinaluan9794@gmail.com>
2017-10-24 10:18:40 +1100

    player starting on the boat

---
+++ Assets/Scripts/BoatScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatScript : MonoBehaviour {
+	
+	private float speed;
+	private readonly float defaultSpeed = 10f;
+
+	private bool isMoving;
+
+	// Use this for initialization
+	void Start () {
+		speed = defaultSpeed;
+		isMoving = true;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+		if (isMoving) {
+			this.transform.localPosition += transform.forward * speed * Time.deltaTime;
+		}
+
+//		if () {
+//			isMoving = false;
+//		}
+	}
+}
